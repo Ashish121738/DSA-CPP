b0bad4b9cca45dc39aaa152e20533954acaa3f08
Ashish121738 <ashishjha3883@gmail.com>
2026-09-17 10:17:51 +0530

    selection sort and buuble sort

---
+++ Array/Bubble_sorting.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout<<"Enter number of elements: ";
+    cin>>n;
+    int arr[1000];
+    cout<<"Enter elements in array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            if(arr[j]>arr[j+1])
+            swap(arr[j],arr[j+1]);
+
+        }
+    }
+    //or
+    // for(int i=0;i<n-1;i++){
+    //     for(int j=0;j<=n-2-i;j++){
+    //         if(arr[j]>arr[j+1])
+    //         swap(arr[j],arr[j+1]);
+    //     }
+
+    // }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
---
+++ Array/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+or(int i=n-2;i>=0;i--){
+    //     for(int j=0;j<=i;j++){
+    //         if(arr[j]>arr[j+1])
+    //         swap(arr[j],arr[j+1]);
+
+    //     }
+    // }
